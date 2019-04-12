1d09aed8fdd044a5baef4fd72113476ba4b77795
Ivan Szebenszki <szib77@gmail.com>
2019-04-12 14:09:16 +0100

    Add more seed data

--- tools/console.rb
+++ tools/console.rb
@@ -1,18 +1,25 @@
 require_relative '../config/environment.rb'
 
-crumble = Recipe.new("Crumble")
-soup = Recipe.new("Soup")
+crumble = Recipe.new('Crumble')
+soup = Recipe.new('Soup')
+pizza = Recipe.new('Pizza')
+taco = Recipe.new('Taco')
 
-ivan = User.new("Ivan")
-harriet = User.new("Harriet")
+ivan = User.new('Ivan')
+harriet = User.new('Harriet')
+garfield = User.new('Garfield')
 
-tomato = Ingredient.new("Tomato")
-apple = Ingredient.new("Apple")
-
-rc1 = RecipeCard.new(crumble, ivan, "02/04/2019", 5)
-rc2 = RecipeCard.new(soup, harriet, "15/01/2019", 3)
-rc3 = RecipeCard.new(crumble, harriet, "06/09/2018", 4)
+tomato = Ingredient.new('Tomato')
+apple = Ingredient.new('Apple')
+flour = Ingredient.new('Flour')
+milk = Ingredient.new('Milk')
+cheese = Ingredient.new('Cheese')
 
 crumble.add_ingredients([apple])
+ivan.add_recipe_card(crumble, '02/04/2019', 5)
+harriet.add_recipe_card(soup, '15/01/2019', 3)
+harriet.add_recipe_card(crumble, '06/09/2018', 4)
+harriet.add_recipe_card(pizza, '06/09/2018', 1)
+harriet.add_recipe_card(taco, '06/09/2018', 4)
 
 binding.pry
