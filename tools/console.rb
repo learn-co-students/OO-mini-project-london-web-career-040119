04c96b1106e4d1537e90d3d085dac11c59a0d695
A Hashim <ahashim983@yahoo.com>
2019-04-12 14:21:21 +0100

    Pair programming added classes & base methods

--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,17 @@
 require_relative '../config/environment.rb'
 
+mashed_potato = Recipe.new("Mashed Potato")
+jacket_potato = Recipe.new("Jacket Potato")
+toast = Recipe.new("Bread")
+
+potatoes = Ingredient.new("Potatoes")
+bread_loaf = Ingredient.new("Bread Loaf")
+butter = Ingredient.new("Butter")
+
+recipe_ingredient_1 = RecipeIngredient.new(potatoes, mashed_potato)
+recipe_ingredient_2 = RecipeIngredient.new(butter, mashed_potato)
+recipe_ingredient_3 = RecipeIngredient.new(bread_loaf, toast)
+
 binding.pry
+
+"hi"
